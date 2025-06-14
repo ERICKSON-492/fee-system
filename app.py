053472aa897c1afb595669c1d603f7bc32efee05
ERICKSON-492 <ericksonagengo@gmail.com>
2025-06-14 19:29:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -244,4 +244,7 @@ def report_outstanding_balance():
     ''')
     return render_template('reports_outstanding_balance.html', data=data)
     if __name__ == "__main__":
-         app.run(debug=True)
+         
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port or default to 5000
+    app.run(host="0.0.0.0", port=port)
